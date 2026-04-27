7e15ee7dbb71c38654f67845c987957cf670490c
Ana Júlia Bock Medina <167197623+Anajuliabm248@users.noreply.github.com>
2026-04-27 12:38:23 -0300

    Criação da tabela de contatos e visualização

--- contact/admin.py
+++ contact/admin.py
@@ -6,10 +6,10 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'email', 'created_date', 'category',
+    list_display = 'id', 'first_name', 'last_name', 'email', 'created_date', 'category', 'show',
     ordering = 'id',
     search_fields = 'first_name', 'id',
-    list_editable = 'first_name', 'last_name', 'email', 'category',
+    list_editable = 'first_name', 'last_name', 'email', 'category', 'show',
     list_per_page = 5
     list_max_show_all = 100
 
--- utils/create_contacts.py
+++ utils/create_contacts.py
@@ -11,7 +11,7 @@ DJANGO_BASE_DIR = Path(__file__).parent.parent
 NUMBER_OF_OBJECTS = 1000 #vão ser criados 1000 objetos
 
 sys.path.append(str(DJANGO_BASE_DIR))
-os.environ["DJANGO_SETTINGS_MODULE"] = 'project.settings'
+os.environ["DJANGO_SETTINGS_MODULE"] = 'Agenda.settings'
 settings.USE_TZ = False
 
 django.setup()
@@ -34,13 +34,14 @@ if __name__ == '__main__':
     django_contacts = []
 
     for _ in range(NUMBER_OF_OBJECTS):  #cria um dicionario de contatos com o faker
-        profile = fake,profile()
+        profile = fake.profile()
         email = profile['mail']
         first_name, last_name = profile['name'].split(' ', 1)
         phone = fake.phone_number()
         created_date: datetime = fake.date_this_year()
-        descripition = random_text = fake.text(max_nb_chars=100)
+        description = random_text = fake.text(max_nb_chars=100)
         category = choice(django_categories)  #escolhe a categoria aleatoriamente
+        print(first_name, email)
 
         django_contacts.append(
             Contact(
@@ -49,12 +50,12 @@ if __name__ == '__main__':
                 phone=phone,
                 email=email,
                 created_date=created_date,
-                descripition=descripition,
+                description=description,
                 category=category,
             )
         )
 
-        if len(django_contacts) > 0:  #se existem contatos cria todos os eles
+    if len(django_contacts) > 0:  #se existem contatos cria todos os eles
             Contact.objects.bulk_create(django_contacts)
 
 
